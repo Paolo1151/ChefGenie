b9e24bc5decaa36cf9c88769ceb688008d7b3022
Paolo Dano <paolodano1@gmail.com>
2022-03-03 14:31:52 +0800

    Initial Commit

---
+++ meal_recommendation_engine/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MealRecommendationEngineConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meal_recommendation_engine'
