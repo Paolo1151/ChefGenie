14daf7ede100a7a2a4c23168d74e0d069fcbf728
ElijahAlcibar <elijah.alcibar@obf.ateneo.edu>
2022-03-12 21:39:43 +0800

    - Updated the views.py to use sessions (please double check idk if I did it right) - Updated some things in the styles.css - Added the signup button

--- login/views.py
+++ login/views.py
@@ -5,40 +5,37 @@ from .models import User
 from .forms import LoginForm
 
 #variables
-user_index = 0
-error_message = ''
 
 # Create your views here
 def account_view(request):
-    if user_index != 0:
-        user = User.objects.get(id=user_index)
+    if request.session['user_index'] != 0:
+        user = User.objects.get(id=request.session['user_index'])
         context = {
-            'user_index': user_index,
+            'user_index': request.session['user_index'],
             'current_user': user
         }
     else:
         context = {
-            'user_index': user_index
+            'user_index': request.session['user_index']
         }
     return render(request, "account/account.html", context)
 
 def login_view(request):
-    global error_message
     if request.method == 'POST':
         username = request.POST['username_field']
         password = request.POST['password_field']
-        error_message = ''
 
         for i in User.objects.all():
             user = User.objects.get(id=i.pk)
             if user.username == username and user.password == password:
-                global user_index 
-                user_index = i.pk
+                request.session['user_index'] = i.pk
                 return redirect('account')
-        error_message = 'invalid username or password'
+        request.session['error_message'] = 'invalid username or password'
+    else:
+        request.session['error_message'] = ''
     context = {
-        'error_message': error_message,
-        'user_index': user_index,
+        'error_message': request.session['error_message'],
+        'user_index': request.session['user_index'],
     }
     return render(request, "login/login.html", context)
 
